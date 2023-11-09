d6f0bff1dc4de1fc205987476956af371db12f3b
Jeremy Evans <code@jeremyevans.net>
2023-11-08 20:33:06 -0800

    Remove tests related to constraint violations
    Spark does not support check, unique, foreign key constraint
    syntax, and seems to ignore NOT NULL constraints.

--- test/database_test.rb
+++ test/database_test.rb
@@ -29,58 +29,6 @@ describe Sequel::Database do
     e.sql.must_equal "SELECT"
   end if ENV['SEQUEL_ERROR_SQL']
 
-  describe "constraint violations" do
-    before do
-      @db.drop_table?(:test2, :test)
-    end
-    after do
-      @db.drop_table?(:test2, :test)
-    end
-
-    cspecify "should raise Sequel::UniqueConstraintViolation when a unique constraint is violated", [:jdbc, :sqlite] do
-      @db.create_table!(:test){String :a, :unique=>true, :null=>false}
-      @db[:test].insert('1')
-      proc{@db[:test].insert('1')}.must_raise(Sequel::UniqueConstraintViolation)
-      @db[:test].insert('2')
-      proc{@db[:test].update(:a=>'1')}.must_raise(Sequel::UniqueConstraintViolation)
-    end
-
-    cspecify "should raise Sequel::UniqueConstraintViolation when a unique constraint is violated for composite primary keys", [:jdbc, :sqlite] do
-      @db.create_table!(:test){String :a; String :b; primary_key [:a, :b]}
-      @db[:test].insert(:a=>'1', :b=>'2')
-      proc{@db[:test].insert(:a=>'1', :b=>'2')}.must_raise(Sequel::UniqueConstraintViolation)
-      @db[:test].insert(:a=>'3', :b=>'4')
-      proc{@db[:test].update(:a=>'1', :b=>'2')}.must_raise(Sequel::UniqueConstraintViolation)
-    end
-
-    cspecify "should raise Sequel::CheckConstraintViolation when a check constraint is violated", [proc{|db| !db.mariadb? || db.server_version <= 100200}, :mysql], [proc{|db| db.sqlite_version < 30802}, :sqlite] do
-      @db.create_table!(:test){String :a; check Sequel.~(:a=>'1')}
-      proc{@db[:test].insert('1')}.must_raise(Sequel::CheckConstraintViolation)
-      @db[:test].insert('2')
-      proc{@db[:test].insert('1')}.must_raise(Sequel::CheckConstraintViolation)
-    end
-
-    cspecify "should raise Sequel::ForeignKeyConstraintViolation when a foreign key constraint is violated", [:jdbc, :sqlite]  do
-      @db.create_table!(:test, :engine=>:InnoDB){primary_key :id}
-      @db.create_table!(:test2, :engine=>:InnoDB){foreign_key :tid, :test}
-      proc{@db[:test2].insert(:tid=>1)}.must_raise(Sequel::ForeignKeyConstraintViolation)
-      @db[:test].insert
-      @db[:test2].insert(:tid=>1)
-      proc{@db[:test2].where(:tid=>1).update(:tid=>3)}.must_raise(Sequel::ForeignKeyConstraintViolation)
-      proc{@db[:test].where(:id=>1).delete}.must_raise(Sequel::ForeignKeyConstraintViolation)
-    end
-
-    cspecify "should raise Sequel::NotNullConstraintViolation when a not null constraint is violated", [:jdbc, :sqlite] do
-      @db.create_table!(:test){Integer :a, :null=>false}
-      proc{@db[:test].insert(:a=>nil)}.must_raise(Sequel::NotNullConstraintViolation)
-      unless @db.database_type == :mysql
-        # Broken mysql silently changes NULL here to 0, and doesn't raise an exception.
-        @db[:test].insert(2)
-        proc{@db[:test].update(:a=>nil)}.must_raise(Sequel::NotNullConstraintViolation)
-      end
-    end
-  end
-
   it "should store underlying wrapped exception in Sequel::DatabaseError" do
     begin
       @db << "SELECT"
